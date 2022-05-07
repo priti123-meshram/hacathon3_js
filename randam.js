45c05a1baf15e2248572aacdd8c7840e0254f445
priti123-meshram <80310091+priti123-meshram@users.noreply.github.com>
2022-05-07 12:16:10 +0530

    Add files via upload

---
+++ randam.js
@@ -0,0 +1,13 @@
+// var color=["blue","pink","orange"]
+// var button =Document.getElementById("button")
+// var myArray = ["blue","HotPink","LightSalmon","Violet","MediumSpringGreen"];
+// var button =  document.getElemen("button");
+// button.addEventListener("click",function(){
+//   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+//   var contenar = document.getElementById("button")
+// })
+
+
+var a="p"
+var b=4
+console.log(a+b)
